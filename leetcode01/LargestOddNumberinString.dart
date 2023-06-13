13f1b20e1068b3b9b5c43cba8d2b4757a25d05bb
haiph-dev <pinkocean.a16@gmail.com>
2023-06-13 19:31:22 +0700

    update

--- leetcode01/LargestOddNumberinString.dart
+++ leetcode01/LargestOddNumberinString.dart
@@ -1,16 +1,14 @@
 void main() {
-  print(largestOdd('52'));
+  print(largestOdd('35427'));
+  print(largestOdd('35472'));
 }
 
-String largestOdd(String n){  
-  try{
-    var nList = n.split('');
-    if (int.parse(nList.last) %2 != 0) return nList.toString();
-
-    List<int> oddList = [];
-    for (int i=0; i<nList.length; i++){
-      if (int.parse(nList[i]) %2 != 0) 
-
+String? largestOdd(String n) {
+  try {
+    for (int i = n.length - 1; i > 0; i--) {
+      if (int.parse(n[i]) % 2 != 0) return n.substring(0, i + 1);
     }
+  } catch (e) {
+    print(e);
   }
 }
